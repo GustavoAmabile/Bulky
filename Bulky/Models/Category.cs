b493763816544c66b66d634028f77eb4847a6c41
Gustavo Amabile <gustavoamabile@outlook.com>
2024-06-07 20:23:47 -0300

    create category

--- Bulky/Models/Category.cs
+++ Bulky/Models/Category.cs
@@ -1,7 +1,18 @@
-﻿namespace Bulky.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models
 {
     public class Category
     {
-        public 
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+        public int DisplayOrder { get; set; }
     }
 }
